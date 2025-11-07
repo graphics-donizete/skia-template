79488fc8b75388b8b2214fe685de89167d17a60c
DonizeteVida <donizetevida1998@outlook.com>
2025-11-07 21:15:18 +0100

    Resolve headers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <skia/include/core/SkPaint.h>
 
 int main(int, char **)
 {
     std::cout << "Hello, from skia_template!\n";
+
+	SkPaint paint;
+	paint.setAntiAlias(true);
+	paint.setColor(SK_ColorRED);
 }
